5daf63e84bdf31741196c6420b52bddaf432f4dc
Avshalom-Z <126082015+Avshalom-Ts@users.noreply.github.com>
2024-01-29 04:57:41 +0200

    Showing my name as first stage

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const renderContent = {
   1: (
-    <h1>1</h1>
+    <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+      Hi, I am <span className='font-semibold'>Avshalom</span>👋
+      <br />
+      A Software Engineer from Israel.
+    </h1>
   ),
   2: (
     <h1>2</h1>
@@ -14,6 +18,12 @@ const renderContent = {
     <h1>4</h1>
   )
 }
+
+const InfoBox = ({ text, link, btnText }) => (
+  <div className='info-box'>
+    {text}
+  </div>
+)
 
 const HomeInfo = ({ currentStage }) => {
   return renderContent[currentStage] || null;
