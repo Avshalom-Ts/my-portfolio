cef786605e0ade72a97459942b09d29f9031e9b1
Avshalom-Z <126082015+Avshalom-Ts@users.noreply.github.com>
2024-01-30 02:43:39 +0200

    Rendering the fox in the page without light

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,9 @@
-import React, { useRef, useState } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+
+import Loader from '../components/Loader'
+import Fox from '../models/Fox'
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -47,63 +51,78 @@ const Contact = () => {
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get it Touch</h1>
-      </div>
-      <form
-        className='w-full flex flex-col gap-7 mt-14'
-        onSubmit={handleSubmit}
-      >
-        <label className='text-black-500 font-semibold'>
-          Name
-          <input
-            type="text"
-            name='name'
-            className='input'
-            placeholder='John'
-            required
-            value={form.name}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </label>
-        <label className='text-black-500 font-semibold'>
-          Email
-          <input
-            type="email"
-            name='email'
-            className='input'
-            placeholder='John@gmail.com'
-            required
-            value={form.email}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </label>
-        <label className='text-black-500 font-semibold'>
-          Your Message
-          <textarea
-            name='message'
-            rows={4}
-            className='textarea'
-            placeholder='Let me know how can i help you!'
-            required
-            value={form.message}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </label>
-        <button
-          type='submit'
-          className='btn'
-          disabled={isLoading}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
+        <form
+          className='w-full flex flex-col gap-7 mt-14'
+          onSubmit={handleSubmit}
         >
-          {isLoading ? 'Sending...' : 'Send Message'}
-        </button>
-      </form>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type="text"
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type="email"
+              name='email'
+              className='input'
+              placeholder='John@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              rows={4}
+              className='textarea'
+              placeholder='Let me know how can i help you!'
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type='submit'
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+
+      {/* Fox Render Section */}
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{ position: [0, 0, 5] }}
+        >
+          <Suspense fallback={<Loader />}>
+            <Fox
+              position={[0.5, 0.35, 0]}
+              rotation={[12, 0, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
 
     </section>
   )
